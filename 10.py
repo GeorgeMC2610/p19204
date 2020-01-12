a0e82db1378fe4bfc520205afdc654d05ac89495
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-01-13 01:52:07 +0200

    Update 10.py

--- 10.py
+++ 10.py
@@ -6,19 +6,25 @@ def whatIsMyHand(hand):
 
     straight    = (hand[0][0] == (hand[1][0] - 1)) and (hand[1][0] == (hand[2][0] - 1)) and (hand[2][0] == (hand[3][0] - 1)) and (hand[3][0] == (hand[4][0] - 1))
     flush       = (hand[0][1] == hand[1][1]) and (hand[1][1] == hand[2][1]) and (hand[2][1] == hand[3][1]) and (hand[3][1] == hand[4][1]) and (hand[4][1] == hand[0][1])
-    royal_flush = (flush) and (straight) and (hand[0][0] == 10)
     kind_4      = ((hand[0][0] == hand[1][0]) and (hand[1][0] == hand[2][0]) and (hand[2][0] == hand[3][0])) or ((hand[1][0] == hand[2][0]) and (hand[2][0] == hand[3][0]) and (hand[3][0] == hand[4][0]))
+    kind_3      = ((hand[0][0] == hand[1][0]) and (hand[1][0] == hand[2][0])) or ((hand[1][0] == hand[2][0]) and (hand[2][0] == hand[3][0])) or ((hand[2][0] == hand[3][0]) and (hand[3][0] == hand[4][0]))
+    kind_2      = ((hand[0][0] == hand[1][0]) or (hand[1][0] == hand[2][0]) or (hand[2][0] == hand[3][0]) or (hand[3][0] == hand[4][0]))
 
     #Ορισμός του χεριού Royal Flush (δηλαδή πέντε χαρτιά ίδιου τύπου σε σειρά, ξεκινώντας από το 10 και φτάνοντας στον άσσο)
-    if royal_flush:
+    if (flush) and (straight) and (hand[0][0] == 10):
         return "Royal Flush"
 
     #Χέρι 'Straight Flush' (χαρτιά σε σειρά με ίδιο τύπο)
     if (flush) and (straight):
         return "Straight Flush"
 
+    #Χέρι Four Of A Kind (self-explanatory)
     if kind_4:
         return "Four Of A Kind"
+
+    #Ορισμός χεριού Full House (ένα ζευγάρι + άλλα τρία ίδια χαρτιά)
+    if kind_3 and kind_2:
+        return "Full House"
 
     #Ορισμός του χεριού FLUSH (Πέντε ίδιοι τύποι)
     if flush:
@@ -28,6 +34,13 @@ def whatIsMyHand(hand):
     if straight:
         return "Straight"
 
+    #Ορισμός του χεριού THREE OF A KIND
+    if kind_3:
+        return "Three Of A Kind"
+
+    #Ορισμός ζευγαριού
+    if kind_2:
+        return "Pair"
     
 
 #Ορισμός των καρτών
@@ -53,9 +66,8 @@ for i in range(0,5):
 player1_cards.sort()
 player2_cards.sort()
 
-player1_cards = [(2, 'Clubs'), (2, 'Spades'), (2, 'Hearts'), (2, 'Diamonds'), (14, 'Clubs')]
+#player1_cards = [(11, 'Hearts'), (6, 'Clubs'), (2, 'Diamonds'), (2, 'Spades'), (2, 'Clubs')]
+print()
 
-print(whatIsMyHand(player1_cards))
-
-print("Player 1 Cards:", player1_cards)
-print("Player 2 Cards:", player2_cards)
+print("Player 1 Cards:", player1_cards, whatIsMyHand(player1_cards))
+print("Player 2 Cards:", player2_cards, whatIsMyHand(player2_cards))
