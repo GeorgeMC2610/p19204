63b9b9ba097f0d15cbe184ccdf324f491f73a438
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-01-12 17:00:11 +0200

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,16 +1,33 @@
 import random
 
 def whatIsMyHand(hand):
-    card_numbers = [2,3,4,5,6,7,8,9,"A","J","Q","K"]
+    card_numbers = [2,3,4,5,6,7,8,9,10,11,12,13,14]
     card_types   = ["Diamonds", "Spades", "Hearts", "Clubs"]
 
-    #Ορισμός του χεριού ROYAL FLUSH
-    for card in hand:
-        if True:
-            break
+    straight    = (hand[0][0] == (hand[1][0] - 1)) and (hand[1][0] == (hand[2][0] - 1)) and (hand[2][0] == (hand[3][0] - 1)) and (hand[3][0] == (hand[4][0] - 1))
+    flush       = (hand[0][1] == hand[1][1]) and (hand[1][1] == hand[2][1]) and (hand[2][1] == hand[3][1]) and (hand[3][1] == hand[4][1]) and (hand[4][1] == hand[0][1])
+    royal_flush = (flush) and (straight) and (hand[0][0] == 10)
+
+    #Ορισμός του χεριού Royal Flush (δηλαδή πέντε χαρτιά ίδιου τύπου σε σειρά, ξεκινώντας από το 10 και φτάνοντας στον άσσο)
+    if royal_flush:
+        return "Royal Flush"
+
+    #Χέρι 'Straight Flush' (χαρτιά σε σειρά με ίδιο τύπο)
+    if (flush) and (straight):
+        return "Straight Flush"
+
+    #Ορισμός του χεριού FLUSH (Πέντε ίδιοι τύποι)
+    if flush:
+        return "Flush"
+    
+    #Ορισμός του χεριού STRAIGHT (Χαρτιά σε σειρά)
+    if straight:
+        return "Straight"
+
+    
 
 #Ορισμός των καρτών
-card_numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+card_numbers = [2,3,4,5,6,7,8,9,10,11,12,13,14]
 card_types   = ["Diamonds", "Spades", "Hearts", "Clubs"]
 cards        = []
 
@@ -31,6 +48,10 @@ for i in range(0,5):
 
 player1_cards.sort()
 player2_cards.sort()
+
+#player1_cards = [(10, 'Clubs'), (11, 'Clubs'), (12, 'Clubs'), (13, 'Clubs'), (14, 'Clubs')]
+
+print(whatIsMyHand(player1_cards))
 
 print("Player 1 Cards:", player1_cards)
 print("Player 2 Cards:", player2_cards)
